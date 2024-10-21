d1abbbb33ac4fffa7821c91af8ae2ecd1d9f7670
prof-carlos-colab <h262.hp@gmail.com>
2024-10-21 12:31:57 +0000

    Configurando conexão com BD

--- app/config/connection.py
+++ app/config/connection.py
@@ -2,5 +2,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
+# Parâmetros de conexão com MySQL.
+db_user = "user"
+db_password = "user_password"
+db_host = "localhost"
+db_port = "3306"
+db_name = "meu_banco"
+
 # URL de conexão para BD MySQL.
-DATABASE_URL = f"mysql+pymysql://usuario:senha@host:porta/nome_bd"
+# DATABASE_URL = f"mysql+pymysql://usuario:senha@host:porta/nome_bd"
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+# Conectando ao banco de dados.
+db = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=db)
+session = Session()
+
+@contextmanager
+def get_db():
+    db = Session() # Cria uma sessão para ações no banco de dados.
+    try:
+        yield db # Caso a sessão realize todas as tarefas, salva a operação.
+        db.commit()
+    except Exception as erro:
+        db.rollback() # Desfaz todas alterações em caso de erro em alguma operação.
+        raise erro # Lança uma exceção.
+    finally:
+        db.close() # Fecha sessão com banco de dados.
